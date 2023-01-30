8e79b2526c7847d3166c6c18f57d160ba3e4f74b
Timur Nabiev <tnabiev82@gmail.com>
2023-01-30 17:02:21 +0500

    refactoring findById method on any classes

--- src/main/java/org/devs/crm/dao/impl/CourseDaoImpl.java
+++ src/main/java/org/devs/crm/dao/impl/CourseDaoImpl.java
@@ -1,5 +1,6 @@
 package org.devs.crm.dao.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.devs.crm.dao.CourseDao;
 import org.devs.crm.model.Course;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,17 +11,32 @@ import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.time.LocalTime;
 
 @Component
+@RequiredArgsConstructor
 public class CourseDaoImpl implements CourseDao {
 
     @Autowired
     public DataSource dataSource;
 
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
     public Course findById(Long id) {
-        return null;
+        String query = "SELECT * FROM tb_courses WHERE id = :id";
+
+        return namedParameterJdbcTemplate.query(query, new MapSqlParameterSource("id", id), (rs, rowNum) -> {
+            System.out.println(rowNum);
+            Course course = new Course();
+            course.setId(rs.getLong("id"));
+            course.setName(rs.getString("name"));
+            course.setSubject(rs.getString("subject"));
+            course.setCourseDurationInMonth(rs.getInt("course_duration_in_month"));
+            course.setLessonDuration(rs.getTime("lesson_duration").toLocalTime());
+            course.setCoursePrice(rs.getBigDecimal("course_price"));
+            return course;
+        }).get(0);
     }
 
     @Override
@@ -34,10 +50,10 @@ public class CourseDaoImpl implements CourseDao {
 
         MapSqlParameterSource source = new MapSqlParameterSource()
                 .addValue("name", course.getName())
-                .addValue("subject",      course.getSubject())
+                .addValue("subject", course.getSubject())
                 .addValue("courseDurationInMonth", course.getCourseDurationInMonth())
-                .addValue("lessonDuration",      course.getLessonDuration())
-                .addValue("coursePrice",course.getCoursePrice());
+                .addValue("lessonDuration", course.getLessonDuration())
+                .addValue("coursePrice", course.getCoursePrice());
 
         namedParameterJdbcTemplate.update(query, source, holder, new String[]{"id"});
 
--- src/main/java/org/devs/crm/dao/impl/MentorDaoImpl.java
+++ src/main/java/org/devs/crm/dao/impl/MentorDaoImpl.java
@@ -1,5 +1,7 @@
 package org.devs.crm.dao.impl;
 
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.devs.crm.dao.MentorDao;
 import org.devs.crm.model.Mentor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +14,28 @@ import org.springframework.stereotype.Component;
 import javax.sql.DataSource;
 
 @Component
+@RequiredArgsConstructor
 public class MentorDaoImpl implements MentorDao {
     @Autowired
     public DataSource dataSource;
 
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     @Override
     public Mentor findById(Long id) {
-        return null;
+        String query = "SELECT * FROM tb_mentors WHERE id = :id";
+
+        return namedParameterJdbcTemplate.query(query, new MapSqlParameterSource("id", id), (rs, rowNum) -> {
+            System.out.println(rowNum);
+            Mentor mentor = new Mentor();
+            mentor.setId(rs.getLong("id"));
+            mentor.setFirstName(rs.getString("first_name"));
+            mentor.setLastName(rs.getString("last_name"));
+            mentor.setPatronymic(rs.getString("patronymic"));
+            mentor.setEmail(rs.getString("email"));
+            mentor.setPhoneNumber(rs.getString("phone_number"));
+            mentor.setSalary(rs.getBigDecimal("salary"));
+            return mentor;
+        }).get(0);
     }
 
     @Override
