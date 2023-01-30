455769e6568b64a76b9e4f548ce6d2e4427a07ff
NodirUmarov <mr.umarov.1995@gmail.com>
2023-01-30 18:52:40 +0500

    refactor

--- src/main/java/org/devs/crm/dao/BaseDao.java
+++
@@ -1,8 +0,0 @@
-package org.devs.crm.dao;
-
-public interface BaseDao<T> {
-
-    T findById(Long id);
-    T save(T t);
-
-}
--- src/main/java/org/devs/crm/dao/MentorDao.java
+++
@@ -1,6 +0,0 @@
-package org.devs.crm.dao;
-
-import org.devs.crm.model.Mentor;
-
-public interface MentorDao extends BaseDao<Mentor>{
-}
