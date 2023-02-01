a0574233c6b604b8cc89ecaff8c57bcb91cdabd9
Timur Nabiev <tnabiev82@gmail.com>
2023-02-01 17:58:17 +0500

    add Query and implement RowMapper classes

--- src/main/java/org/devs/crm/dao/impl/query/StudentQuery.java
+++ src/main/java/org/devs/crm/dao/impl/query/StudentQuery.java
@@ -4,5 +4,5 @@ public class StudentQuery {
     public static final String SELECT_ONE = "SELECT * FROM tb_students WHERE id = :id";
     public static final String SAVE_STUDENT = "" +
             "INSERT INTO tb_students(first_name, last_name, patronymic, email, phone_number) " +
-            "VALUES(:fname, :lname, :patronybic, :email, :phoneNumber)";
+            "VALUES(:fname, :lname, :patronymic, :email, :phoneNumber)";
 }
