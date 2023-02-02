c507ae37f4adad8884a73aa6c91b71c252a82d6e
Skward18 <elbek1987.26.07@mail.ru>
2023-02-02 21:19:23 +0500

    Just

--- src/main/java/org/devs/crm/CrmApplication.java
+++ src/main/java/org/devs/crm/CrmApplication.java
@@ -1,6 +1,7 @@
 package org.devs.crm;
 
 import org.devs.crm.dao.GroupDao;
+import org.devs.crm.dao.MentorDao;
 import org.devs.crm.dao.config.DaoConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -8,8 +9,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class CrmApplication {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoConfig.class);
-        GroupDao groupDao = context.getBean(GroupDao.class);
+        MentorDao mentorDao = context.getBean(MentorDao.class);
 
-        System.out.println(groupDao.findById(1L));
+        System.out.println(mentorDao.findAllByGroupId(1L));
     }
 }
